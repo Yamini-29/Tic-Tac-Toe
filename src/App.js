58394f21fa49e09b86a09d15a6fae1ec6724a861
yaminibaskar29 <cs21b056@iittp.ac.in>
2024-10-10 10:31:40 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TicTacToe from './components/TicTacToe/Tictactoe';
+
+
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
